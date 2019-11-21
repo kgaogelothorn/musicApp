5bdbdf8affbdcfa91ae6b735015a6be88fdba850
Kgaogelothorn <kgaogelothorn@gmail.com>
2019-11-21 16:08:53 +0200

    fix(api):fixed api proxy

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,27 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Config } from '../../config/config';
+import { Config, BaseApi } from '../../config/config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
   options = {headers: {'Content-type': 'application/json', }};
-  baseApi = Config.api ;
+  baseApi = BaseApi;
 
   constructor(readonly http: HttpClient) { }
 
   search(value) {
-    return this.http.get(`/search?q=${value}`);
+    return this.http.get(`${this.baseApi}/search?q=${value}`);
   }
   getArtist(id: string) {
-    return this.http.get(`/artist/${id}`);
+    return this.http.get(`${this.baseApi}/artist/${id}`);
   }
   getTracklist(req: any) {
     return this.http.get(req.replace('https://api.deezer.com', ''));
   }
   getTrackInfo(id: string) {
-    return this.http.get(`/track/${id}`);
+    return this.http.get(`${this.baseApi}/track/${id}`);
   }
 
 }
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,5 @@
 export const Config = {
     api: `https://api.deezer.com`,
-}
+    cors: `https://cors-anywhere.herokuapp.com/`
+};
+export const BaseApi = `${Config.cors}${Config.api}`;
